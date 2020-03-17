d7ea9e4cdf8d6abe77e5290301a0f66ab95aa850
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-03-17 13:05:27 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,10 @@
-sudo ./setup_krb.sh aws templates/iot_workshop.json /dev/xvdf
+#!/bin/sh
+
+# logging stdout/stderr
+set -x
+exec >> /root/bootstrap.log 2>&1
+date
+
+./setup_krb.sh aws templates/iot_workshop.json /dev/xvdf
+
+./scripts/instance-postcreate-gpu-p2-instance.sh
